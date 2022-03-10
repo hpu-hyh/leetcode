87d4c830b7da883566836cb87167e16bc3fa5b93
啥都不会的铁废物 <2787281908@qq.com>
2022-03-10 21:27:16 +0800

    每日一题（3）

---
+++ leetcode(两数之和).js
@@ -0,0 +1,28 @@
+// 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+
+// 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+
+// 你可以按任意顺序返回答案。
+
+// 输入：nums = [2,7,11,15], target = 9
+// 输出：[0,1]
+// 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+
+// 编码实现：
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+ var twoSum = function(nums, target) {
+    let arr = [];
+    for (let i = 0;i < nums.length ;i++) {
+        for (let j = nums.length; j>i ;j--) {
+            if (nums[i] + nums[j] == target) {
+                arr.push(i);
+                arr.push(j);
+            }
+        }
+    }
+    return arr;
+};
