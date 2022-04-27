a413f845560db8bee6d92d125564d7b55d8542c6
hpu-hyh <2787281908@qq.com>
2022-04-27 22:29:15 +0800

    Create leetcode(有效的字母异味词).js

---
+++ 每日一题/leetcode(有效的字母异味词).js
@@ -0,0 +1,32 @@
+// 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+
+// 注意：若 s 和 t 中每个字符出现的次数都相同，则称 s 和 t 互为字母异位词。
+
+//  
+
+// 示例 1:
+
+// 输入: s = "anagram", t = "nagaram"
+// 输出: true
+// 示例 2:
+
+// 输入: s = "rat", t = "car"
+// 输出: false
+
+//代码实现：
+var isAnagram = function(s, t) {
+    if(s.length!==t.length){
+            return false;
+        }
+       if(s.length==t.length&&[...s].sort().join('')==[...t].sort().join('')){
+           return true;
+       }else{
+           return false;
+       }
+    };
+
+
+//简洁版：
+var isAnagram = function(s, t) {
+    return s.length == t.length && [...s].sort().join('') === [...t].sort().join('')
+};
