c1bb40b1dc43a963ec9d37bdbda61853c9f87c8c
hpu-hyh <2787281908@qq.com>
2022-04-28 21:33:54 +0800

    Create leetcode( 数组中出现次数超过一半的数字).js

---
+++ 每日一题/leetcode( 数组中出现次数超过一半的数字).js
@@ -0,0 +1,16 @@
+// 数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。
+
+
+// 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+
+
+// 示例 1:
+// 输入: [1, 2, 3, 2, 2, 2, 5, 4, 2]
+// 输出: 2
+
+//代码实现：
+var majorityElement = function(nums) {
+    nums.sort();
+    const idx = Math.floor(nums.length/2)
+    return nums[idx];
+};
